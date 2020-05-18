17bbfe013791d8807c4d855d23583c8ac7791685
Jack Works <jackworks@protonmail.com>
2020-05-18 16:48:57 +0800

    feat: support new facebook ui

--- src/social-network-provider/facebook.com/tasks/getPostContent.ts
+++ src/social-network-provider/facebook.com/tasks/getPostContent.ts
@@ -1,8 +1,12 @@
 import { LiveSelector, MutationObserverWatcher } from '@holoflows/kit/es'
 import { timeout } from '../../../utils/utils'
 
+const oldFacebook = new LiveSelector().querySelector('#contentArea').getElementsByTagName('p')
+const newFacebook = new LiveSelector().querySelector<HTMLElement>('[data-ad-preview="message"]')
 export async function getPostContentFacebook(): Promise<string> {
-    const post = new LiveSelector().querySelector('#contentArea').getElementsByTagName('p')
+    return get(oldFacebook.concat(newFacebook))
+}
+async function get(post: LiveSelector<HTMLElement>) {
     const [data] = await timeout(new MutationObserverWatcher(post), 10000)
     return data.innerText
 }
